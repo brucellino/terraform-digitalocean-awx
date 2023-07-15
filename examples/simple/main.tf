9e3c51aed2164e2bd493b69b366c3b72863cd061
Bruce Becker <brucellino@protonmail.com>
2023-07-15 09:19:27 +0200

    refactor(build): move variables out of main definition
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -8,8 +8,23 @@ terraform {
   backend "consul" {
     path = "terraform/modules/digitalocean-awx/simple"
   }
+
+}
+module "vpc" {
+  source          = "brucellino/vpc/digitalocean"
+  version         = "1.0.3"
+  vpc_description = var.vpc.description
+  vpc_name        = var.vpc.name
+  vpc_region      = var.vpc.region
+  project         = var.project
 }
 
+module "k8s" {
+  source       = "brucellino/kubernetes/digitalocean"
+  version      = "2.0.0"
+  vpc_name     = var.vpc.name
+  project_name = var.project.name
+}
 
 module "example" {
   source = "../../"
