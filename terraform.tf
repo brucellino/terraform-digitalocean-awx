f7bb85828718ac740de67ae008da9450a6b037f9
Bruce Becker <brucellino@protonmail.com>
2023-07-15 08:49:59 +0200

    chore: initial commit

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,5 @@
-# Use this file to declare the terraform configuration
-# Add things like:
-# - required version
-# - required providers
-# Do not add things like:
-# - provider configuration
-# - backend configuration
-# These will be declared in the terraform document which consumes the module.
-
 terraform {
-  required_version = ">1.2.0"
+  required_version = "> 1.5"
   required_providers {
     # Add your required providers here.
   }
